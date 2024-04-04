9a2cfffe509490c9f7aa11fb9eb82ebe774e2cf7
anusha <konakanchi3anusha@gmail.com>
2024-04-04 07:03:08 +0000

    solution

---
+++ src/components/Login/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Login = props => {
+  const {login} = props
+  return (
+    <button type="button" className="btn" onClick={login}>
+      Login
+    </button>
+  )
+}
+export default Login
